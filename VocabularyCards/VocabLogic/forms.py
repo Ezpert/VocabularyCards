a22365439ec43c443ffab953dccc63d1ce428d0c
Ezpert <109883612+Ezpert@users.noreply.github.com>
2024-04-24 19:01:58 -0500

    Forms

--- VocabularyCards/VocabLogic/forms.py
+++ VocabularyCards/VocabLogic/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 
 class CardFullForm(forms.Form):
-    word = forms.CharField(label='Word', max_length=100)
-    definition = forms.CharField(label='Definition', max_length=300)
-    sentence = forms.CharField(label='Sentence', max_length=250)
+    word = forms.CharField(label='Word', max_length=100,  widget=forms.TextInput(attrs={"class": "form-word"}))
+    definition = forms.CharField(label='Definition', max_length=300,  widget=forms.TextInput(attrs={"class": "form-def"}))
+    sentence = forms.CharField(label='Sentence', max_length=250,  widget=forms.TextInput(attrs={"class": "form-sen"}))
 
 
 class CardEmptyForm(forms.Form):
