038852dcc3456334a21c41a729dc3bb92733da54
Ezpert <109883612+Ezpert@users.noreply.github.com>
2024-06-16 23:42:32 -0500

    Added Modal to edit Page, Fixed editing UI. Finished UI for viewCards.css

--- VocabularyCards/VocabLogic/views.py
+++ VocabularyCards/VocabLogic/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from openai import OpenAI
-from .forms import CardEmptyForm, CardFullForm, EditDefForm
+from django.views.decorators.http import require_http_methods, require_POST
+from .forms import CardEmptyForm, CardFullForm
 from .models import Card, Definition
 from django.urls import reverse
 import requests
-from django.core.cache import cache
 import os
 from dotenv import load_dotenv
 
@@ -19,7 +19,7 @@ client = OpenAI(
 
 def chat_completion(
         message,
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         prompt="You are a helpful assistant.",
         temperature=0,
         messages=[],
@@ -107,21 +107,18 @@ def ai_example(word=None, definition=None):
     """
     outputs = {}
     message = f"Word: {word} + \n Definition: {definition}"
-    response = chat_completion(message, prompt=prompt, model='gpt-3.5-turbo')
-
+    response = chat_completion(message, prompt=prompt, model='gpt-4')
 
     print(response)
     return response
 
 
+@require_POST
 def makeSen(request):
-    if request.method == 'POST':
-        word_example = request.POST.get('word_example')
-        definition_example = request.POST.get('definition_example')
-        new_sentence = ai_example(word=word_example, definition=definition_example)
-        return JsonResponse({'new_sentence': new_sentence})
-    else:
-        return HttpResponseNotAllowed(['POST'])
+    word_example = request.POST.get('word_example')
+    definition_example = request.POST.get('definition_example')
+    new_sentence = ai_example(word=word_example, definition=definition_example)
+    return JsonResponse({'new_sentence': new_sentence})
 
 
 # Create your views here.
@@ -151,177 +148,165 @@ def createPage(request):
     return render(request, 'createPage.html', {'f_form': f_form, 'e_form': e_form})
 
 
+@require_POST
 def addCard(request):
-    if request.method == 'POST':
-        print("Valid method!")
-        if 'f_form' in request.POST:
+    print("Valid method!")
+    if 'f_form' in request.POST:
+        print("Form f found!")
+        form_f = CardFullForm(request.POST)
+        if form_f.is_valid():
+            print("Valid form!")
+            try:
+                print("Working!")
+                # Use get_or_create to find or create a Card instance with the word from the form
+                new_card, created = Card.objects.get_or_create(
+                    word=form_f.cleaned_data['word'].lower()
+                )
 
-            print("Form f found!")
-            form_f = CardFullForm(request.POST)
-            if form_f.is_valid():
-                print("Valid form!")
-                try:
-                    print("Working!")
-                    # Use get_or_create to find or create a Card instance with the word from the form
-                    new_card, created = Card.objects.get_or_create(
-                        word=form_f.cleaned_data['word'].lower()
-                    )
+                # Create a new Card instance with the word from the form
+                if created:
+                    new_card.save()
 
-                    # Create a new Card instance with the word from the form
-                    if created:
-                        new_card.save()
+                definition_text = form_f.cleaned_data['definition']
+                sentence_use = form_f.cleaned_data['sentence']
+                new_definition = Definition(card=new_card, definition_text=definition_text,
+                                            sentence_use=sentence_use)
+                new_definition.save()
+                return JsonResponse({'status': 'success'}, status=200)
+            except Definition.DoesNotExist:
 
-                    definition_text = form_f.cleaned_data['definition']
-                    sentence_use = form_f.cleaned_data['sentence']
-                    new_definition = Definition(card=new_card, definition_text=definition_text,
-                                                sentence_use=sentence_use)
-                    new_definition.save()
-                    return JsonResponse({'status': 'success'}, status=200)
-                except Definition.DoesNotExist:
+                return JsonResponse({'status': 'Definition not found'}, status=404)
+            except Card.DoesNotExist:
+                return JsonResponse({'status': 'Card not found'}, status=404)
 
-                    return JsonResponse({'status': 'Definition not found'}, status=404)
-                except Card.DoesNotExist:
-                    return JsonResponse({'status': 'Card not found'}, status=404)
+    elif 'e_form' in request.POST:
+        print("Form e found!")
+        form_e = CardEmptyForm(request.POST)
+        if form_e.is_valid():
+            # Attempt to get or create the card
+            wordF = form_e.cleaned_data['word'].lower()
+            # Make a search for the definition of the word and using it in a sentence
+            url = f'https://api.dictionaryapi.dev/api/v2/entries/en/{wordF}'
+            response = requests.get(url)
 
-        elif 'e_form' in request.POST:
-            print("Form e found!")
-            form_e = CardEmptyForm(request.POST)
-            if form_e.is_valid():
-                # Attempt to get or create the card
-                wordF = form_e.cleaned_data['word'].lower()
-                # Make a search for the definition of the word and using it in a sentence
-                url = f'https://api.dictionaryapi.dev/api/v2/entries/en/{wordF}'
-                response = requests.get(url)
+            # if the request is successful
+            if response.status_code == 200:
+                # Convert the response to JSON
+                new_card = form_e.cleaned_data['word'].lower()
+                data = response.json()
 
-                # if the request is successful
-                if response.status_code == 200:
-                    # Convert the response to JSON
-                    new_card = form_e.cleaned_data['word'].lower()
-                    data = response.json()
+                definitions_and_examples = []
+                for item in data:
+                    for meaning in item['meanings']:
+                        for definition in meaning['definitions']:
+                            example = definition.get('example', 'No example provided')
+                            if example == 'No example provided':
+                                example = 'No example Provided'
+                                # example = ai_example(word=new_card, definition=definition['definition'])
+                            definitions_and_examples.append({
+                                'word': new_card,
+                                'definition': definition['definition'],
+                                'example': example
 
-                    definitions_and_examples = []
-                    for item in data:
-                        for meaning in item['meanings']:
-                            for definition in meaning['definitions']:
-                                example = definition.get('example', 'No example provided')
-                                if example == 'No example provided':
-                                    example = 'No example Provided'
-                                    # example = ai_example(word=new_card, definition=definition['definition'])
-                                definitions_and_examples.append({
-                                    'word': new_card,
-                                    'definition': definition['definition'],
-                                    'example': example
+                            })
 
-                                })
+                context = {'definitions_and_examples': definitions_and_examples}
+                return render(request, 'wordSearch.html', context)
 
-                    context = {'definitions_and_examples': definitions_and_examples}
-                    return render(request, 'wordSearch.html', context)
-
-                else:
-                    # Handle the case where the API request failed
-                    print("Failed to fetch data from the URL.")
-                    return redirect('createPage')
             else:
-                return render(request, 'landing.html')
+                # Handle the case where the API request failed
+                print("Failed to fetch data from the URL.")
+                return redirect('createPage')
         else:
-            print("Form f or e not found!")
-            return JsonResponse({'status': 'Form not found'}, status=404)
+            return render(request, 'landing.html')
     else:
-        return HttpResponseNotAllowed(['POST'])
+        print("Form f or e not found!")
+        return JsonResponse({'status': 'Form not found'}, status=404)
 
 
+@require_POST
 def searchAdd(request):
     print("Request received")  # Debugging line
-    if request.method == 'POST':
-        print('Method: POST')
-        word = request.POST.get('word')
-        definition = request.POST.get('definition')
-        sentence = request.POST.get('sentence')
+    print('Method: POST')
+    word = request.POST.get('word')
+    definition = request.POST.get('definition')
+    sentence = request.POST.get('sentence')
 
-        print(f'Word: {word} Definition: {definition} Sentence: {sentence}')
+    print(f'Word: {word} Definition: {definition} Sentence: {sentence}')
 
-        try:
+    try:
 
-            new_card, created = Card.objects.get_or_create(
-                word=word.lower()
-            )
-            if created:
-                new_card.save()
+        new_card, created = Card.objects.get_or_create(
+            word=word.lower()
+        )
+        if created:
+            new_card.save()
 
-            new_def, createdDef = Definition.objects.get_or_create(
-                card_id=new_card.id,
-                definition_text=definition,
-                sentence_use=sentence
-            )
+        new_def, createdDef = Definition.objects.get_or_create(
+            card_id=new_card.id,
+            definition_text=definition,
+            sentence_use=sentence
+        )
 
-            if createdDef:
-                new_def.sentence_use = sentence
-                new_def.save()
+        if createdDef:
+            new_def.sentence_use = sentence
+            new_def.save()
 
-            return JsonResponse({'status': 'success'}, status=200)
-        except Definition.DoesNotExist:
-            return JsonResponse({'status': 'Definition not found'}, status=404)
-        except Card.DoesNotExist:
-            return JsonResponse({'status': 'Card not found'}, status=404)
-    else:
-        print('Method: GET')
-        return HttpResponseNotAllowed(['POST'])
+        return JsonResponse({'status': 'success'}, status=200)
+    except Definition.DoesNotExist:
+        return JsonResponse({'status': 'Definition not found'}, status=404)
+    except Card.DoesNotExist:
+        return JsonResponse({'status': 'Card not found'}, status=404)
 
 
+@require_POST
 def deleteCard(request, card_id):
-    if request.method == 'POST':
-        Card.objects.filter(pk=card_id).delete()
-        redirect_url = reverse('viewCards')
-        response_data = {'redirect_url': redirect_url}
-
-        return JsonResponse(response_data)
-    else:
-        return HttpResponseNotAllowed(['POST'])
+    Card.objects.filter(pk=card_id).delete()
+    redirect_url = reverse('viewCards')
+    response_data = {'redirect_url': redirect_url}
+    return JsonResponse(response_data)
 
 
+@require_POST
 def deleteDef(request, def_id):
-    if request.method == 'POST':
-        try:
-            definition = Definition.objects.get(pk=def_id)
-            card_id = definition.card.id
-            definition.delete()
-            return redirect('viewCard', card_id=card_id)
-        except Definition.DoesNotExist:
-            return redirect('viewCards')  # Redirect to 'viewCards' if the definition does not exist
-    else:
-        return redirect('viewCards')  # Redirect to 'viewCards' for non-POST requests
+    try:
+        print("Request received")
+        definition = Definition.objects.get(pk=def_id)
+        card_id = definition.card.id
+        definition.delete()
+        return redirect('viewCard', card_id=card_id)
+    except Definition.DoesNotExist:
+        return redirect('viewCards')  # Redirect to 'viewCards' if the definition does not exist
 
 
+@require_POST
 def editWord(request, card_id):
     print("Request received")  # Debugging line
-    if request.method == 'POST':
-        card_id = request.POST.get('card-id')
-        def_id = request.POST.get('def-id')
-        word_text = request.POST.get('word-text')
-        def_text = request.POST.get('def-text')
-        sentence = request.POST.get('sentence-use')
+    card_id = request.POST.get('card-id')
+    def_id = request.POST.get('def-id')
+    word_text = request.POST.get('word-text')
+    def_text = request.POST.get('def-text')
+    sentence = request.POST.get('sentence-use')
 
-        print(
-            f"Card ID: {card_id},"
-            f" Word Text: {word_text},"
-            f" Definition ID: {def_id}"
-            f", Definition: {def_text}, "
-            f"Sentence: {sentence}")
+    print(
+        f"Card ID: {card_id},"
+        f" Word Text: {word_text},"
+        f" Definition ID: {def_id}"
+        f", Definition: {def_text}, "
+        f"Sentence: {sentence}")
 
-        try:
-            card = Card.objects.get(pk=card_id)
-            card.word = word_text.lower()
-            definition = Definition.objects.get(pk=def_id)
-            definition.definition_text = def_text
-            definition.sentence_use = sentence
+    try:
+        card = Card.objects.get(pk=card_id)
+        card.word = word_text.lower()
+        definition = Definition.objects.get(pk=def_id)
+        definition.definition_text = def_text
+        definition.sentence_use = sentence
 
-            card.save()
-            definition.save()
+        card.save()
+        definition.save()
 
-            return JsonResponse({'status': 'success'}, status=200)
-        except Definition.DoesNotExist:
-            return JsonResponse({'status': 'Definition not found'}, status=404)
-        except Card.DoesNotExist:
-            return JsonResponse({'status': 'Card not found'}, status=404)
-    else:
-        return HttpResponseNotAllowed(['POST'])
+        return JsonResponse({'status': 'success'}, status=200)
+    except Definition.DoesNotExist:
+        return JsonResponse({'status': 'Definition not found'}, status=404)
+    except Card.DoesNotExist:
+        return JsonResponse({'status': 'Card not found'}, status=404)
